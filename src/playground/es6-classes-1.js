5c4399db011584937639f19fd15f2eb5e6266625
sansaristic <jaysansa@gmail.com>
2017-12-12 23:14:05 +0100

    es6 class practise

---
+++ src/playground/es6-classes-1.js
@@ -0,0 +1,18 @@
+class Person {
+    constructor(name = 'Anonymous', age = 0) {
+        this.name = name;
+        this.age = age;
+    }
+
+    getGreeting() {
+        return `Hi. I am ${this.name}!`;
+    }
+
+    getDescription() {
+        return `${this.name} is ${this.age} year(s) old.`;
+    }
+}
+
+const me = new Person('Nouwatin Jacob', 34);
+console.log(me.getGreeting());
+console.log(me.getDescription());

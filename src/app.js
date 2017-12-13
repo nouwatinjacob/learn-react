0621453eb6ab2946ffaca3a29cdd9295abf622be
sansaristic <jaysansa@gmail.com>
2017-12-13 01:02:11 +0100

    spliting the indecision components

--- src/app.js
+++ src/app.js
@@ -1,53 +1,57 @@
-console.log('App.js is running!');
-
-const user = {
-    title: 'React Tutorial',
-    subtitle: 'This is just the beginning',
-    options: ['Shola', 'Adebisi', 'Iwofa']
-};
-
-const onFormSubmit = (e) => {
-  e.preventDefault();
-
-  const option = e.target.elements.option.value;
-  
-  if (option) {
-    user.options.push(option);
-    e.target.elements.option.value = '';
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Indecision App</h1>
+        <h2>Put your life in the hands of a Computer</h2>
+      </div>
+    );
   }
-  submitReaction();  
-};
-
-const removeAll = () => {
-  user.options.length = 0;
-  submitReaction();
-};
-
-const appRoot = document.getElementById('app');
-
-const submitReaction = () => {
-  const template = (
-    <div>
-        <h1>{user.title}</h1>
-        {user.subtitle && <p>{user.subtitle}</p>}
-        {user.options.length > 0 ? <p>Here are your Options</p> : <p>No Option</p>}
-        <p>{user.options.length}</p>
-        <ol>
-            {
-              user.options.map((option) => {
-                return <li key={option}>{option}</li>;
-              })
-            }
-        </ol>
-        <form onSubmit={onFormSubmit}>
-          <input type="text" name="option"/>
-          <button>Add Option</button>
-          <button onClick={removeAll}>Remove All</button>
-        </form>
-    </div>
-  );
-  ReactDOM.render(template, appRoot);
 }
 
-submitReaction();
+class Action extends React.Component {
+  render() {
+    return (
+      <div>
+        <button>What should I do?</button>
+      </div>
+    );
+  }
+}
 
+class Options extends React.Component {
+  render() {
+    return (
+      <div>
+        <ol>
+          <li>Pray in the Morning</li>
+          <li>Exercise the body</li>
+        </ol>
+      </div>
+    );
+  }
+}
+
+class AddOption extends React.Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <input type="text" name="option"/>
+          <button>Add Option</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+const jsx = (
+  <div>
+  <Header />
+  <Action />
+  <Options />
+  <AddOption />
+  </div>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));

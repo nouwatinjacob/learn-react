db560d766da2ac7e0d50488267c748bd0a2f0e22
sansaristic <jaysansa@gmail.com>
2017-12-15 19:52:33 +0100

    Saving and Loading Counter

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -5,13 +5,28 @@ class Counter extends React.Component {
         this.handleMinusOne = this.handleMinusOne.bind(this);
         this.handleReset = this.handleReset.bind(this);
         this.state ={
-          count: props.count
+          count: 0
         };
     }
+
+    componentDidMount() {
+      const count = localStorage.getItem('count');
+
+      if (!isNaN(count)) {}
+        this.setState(() => ({ count }));
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+      if (prevState.count !== this.state.count) {
+        const num = parseInt(this.state.count, 10);
+        localStorage.setItem('count', num);
+      }
+    }
+
     handleAddOne() {
         this.setState((prevState) => {
           return {
-            count: prevState.count + 1
+            count: parseInt(prevState.count) + 1
           };
         });
     }
@@ -19,7 +34,7 @@ class Counter extends React.Component {
     handleMinusOne() {
       this.setState((prevState) => {
         return {
-          count: prevState.count - 1
+          count: parseInt(prevState.count) - 1
         };
       });        
     }
@@ -43,9 +58,5 @@ class Counter extends React.Component {
         );
     }
 }
-
-Counter.defaultProps = {
-  count: 0
-};
 
 ReactDOM.render(<Counter />, document.getElementById('app'));
